b34b5054e712bf111777040394006eceb9df3de6
Achilleas Seisa <achilleas2942@gmail.com>
2025-02-10 13:32:38 +0100

    Pass as parameters ROS_MASTER_URI and ROS_IP

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -8,4 +8,4 @@ docker run --privileged -it \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     ghcr.io/achilleas2942/multiple_turtles_and_pelicans_simulator \
-    bash
+    6 130.240.96.104 130.240.96.104 # Change the number of robots, ROS_MASTER_IP, and ROS_IP as needed
