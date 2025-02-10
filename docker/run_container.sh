3afa22a71498d49882e92c35c595b9478378a8bc
Achilleas Seisa <achilleas2942@gmail.com>
2025-02-10 16:07:15 +0100

    Format clean

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -33,9 +33,9 @@ fi
 
 # Run the Docker container with the appropriate environment variables
 docker run --privileged -it \
---net=host \
---env="DISPLAY" \
---env="QT_X11_NO_MITSHM=1" \
---volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-ghcr.io/achilleas2942/multiple_turtles_and_pelicans_simulator \
-"$NUM_ROBOTS" "$ROS_MASTER_IP" "$ROS_IP"
+    --net=host \
+    --env="DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    ghcr.io/achilleas2942/multiple_turtles_and_pelicans_simulator \
+    "$NUM_ROBOTS" "$ROS_MASTER_IP" "$ROS_IP"
