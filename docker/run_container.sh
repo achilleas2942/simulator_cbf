d72211a7d1bd0265e482a0be55d813a29223cedf
Achilleas Seisa <achilleas2942@gmail.com>
2025-02-10 13:40:25 +0100

    Pass number of robots,  ROS_MASTER_URI and ROS_IP, as variables when running the bash script

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,6 +1,21 @@
 # Allow docker container to connect to the display server
 xhost +local:docker
 
+# Default values
+NUM_ROBOTS=4
+ROS_MASTER_IP="130.240.96.104"
+ROS_IP="130.240.96.104"
+
+# Parse command-line arguments
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        robot) NUM_ROBOTS="$2"; shift 2;;
+        ros_master) ROS_MASTER_IP="$2"; shift 2;;
+        ros_ip) ROS_IP="$2"; shift 2;;
+        *) echo "Unknown option: $1"; exit 1;;
+    esac
+done
+
 # Run the Docker container with the appropriate environment variables
 docker run --privileged -it \
     --net=host \
@@ -8,4 +23,4 @@ docker run --privileged -it \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     ghcr.io/achilleas2942/multiple_turtles_and_pelicans_simulator \
-    6 130.240.96.104 130.240.96.104 # Change the number of robots, ROS_MASTER_IP, and ROS_IP as needed
+    "$NUM_ROBOTS" "$ROS_MASTER_IP" "$ROS_IP"
